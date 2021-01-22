fedcfe6d58980511d2480e138a11598715d613f7
Sumit Vajarinkar <vajarinkarss@gmail.com>
2021-01-22 13:40:32 +0530

    Update 13stockbuysell(1).cpp

--- Array/13stockbuysell(1).cpp
+++ Array/13stockbuysell(1).cpp
@@ -4,30 +4,25 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-int maxProfit(int price[], int start, int end)
+int maxProfit(int price[],int start,int end)
 {
-	if(end <= start)
-		return 0;
-
-	int profit = 0;
-
-	for(int i = start; i < end; i++) 
-	{
-		for(int j = i + 1; j <= end; j++)
-		{
-			if(price[j] > price[i])
-			{
-				//recursive approach
-				int curr_profit = price[j] - price[i] 
-								  + maxProfit(price, start, i - 1)
-								  + maxProfit(price, j + 1, end);
-
-				profit = max(profit, curr_profit);
-			}
-		}
-	}
-
-	return profit;
+ 	if(end<=start)
+ 		return 0;
+ 		
+ 	int profit=0;                   //here we compare 1 by 1
+ 	for(int i=start;i<end;i++)
+ 		{
+ 			for(int j=i+1;j<=end;j++)
+ 			{
+ 				if(price[j]>price[i])
+ 				{
+					//recursive approach
+ 					int curr_profit=price[j]-price[i]+ maxProfit(price,start,i-1) + maxProfit(price,j+1,end);
+ 					profit=max(profit,curr_profit);
+				 }
+			 }
+		 }
+		 return profit;
 }
 int main()
 {
@@ -39,6 +34,7 @@ int main()
     cout<<maxProfit(price,0,n);
     return 0;
 }
+
 
 
 
