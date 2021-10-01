7beff17b921e173b863585b9cdca16186f043f49
Sumit Vajarinkar <vajarinkarss@gmail.com>
2021-10-01 16:09:35 +0530

    Add files via upload

---
+++ Heaps/4buildMinHeap.cpp
@@ -0,0 +1,84 @@
+/// Min Heapify
+///O(n log n)
+
+
+//i/p: arr[]=4,10,2,13,7,14,20,1,9
+//o/p:1 4 2 9 7 14 20 13 10
+
+#include <iostream>
+
+using namespace std;
+
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. N is size of heap
+void minHeapify(int arr[], int n, int i)
+
+
+{
+    int l = 2 * i + 1; // left = 2*i + 1
+	int r = 2 * i + 2; // right = 2*i + 2
+    int smallest = i;
+
+    if (l < n && arr[l] < arr[i])
+        smallest = l;
+
+    if (r < n && arr[r] < arr[smallest])
+        smallest = r;
+
+    if (smallest != i)
+    {
+        swap(arr[i], arr[smallest]);
+        minHeapify(arr,n,smallest);
+    }
+}
+
+// Function to build a Max-Heap from the given array
+void buildHeap(int arr[], int n)
+{
+	for (int i = (n-2)/2;i>=0;i--) {
+		minHeapify(arr, n, i);
+	}
+}
+
+// A utility function to print the array
+// representation of Heap
+void printHeap(int arr[], int n)
+{
+	cout << "Array representation of Min Heap is:\n";
+
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+int main()
+{
+	/*
+			   4
+			 /	 \
+	        10    2
+		  / \	 / \
+	     13 7   14 20
+	    / \
+	    1 9
+	*/
+	int arr[] = { 4,10,2,13,7,14,20,1,9 };
+
+
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	buildHeap(arr, n);
+
+	printHeap(arr, n);
+
+	// Final Min Heap:
+	//			1
+	//		 /	 \
+	//     4     2
+	//	 / \	 / \
+	//  9	7   14 20
+	// / \
+	// 13 10
+
+	return 0;
+}
---
+++ Heaps/5heapSort.cpp
@@ -0,0 +1,99 @@
+/// Heap sort
+///O(n log n)
+
+
+//i/p: arr[]=4,10,2,13,7,14,20,1,9
+//o/p: 1 2 4 7 9 10 13 14 20
+
+#include <iostream>
+using namespace std;
+
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. N is size of heap
+void maxHeapify(int arr[], int n, int i)
+{
+	int largest = i; // Initialize largest as root
+	int l = 2 * i + 1; // left = 2*i + 1
+	int r = 2 * i + 2; // right = 2*i + 2
+
+	// If left child is larger than root
+	if (l < n && arr[l] > arr[largest])
+		largest = l;
+
+	// If right child is larger than largest so far
+	if (r < n && arr[r] > arr[largest])
+		largest = r;
+
+	// If largest is not root
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+
+		// Recursively heapify the affected sub-tree
+		maxHeapify(arr, n, largest);
+	}
+}
+
+// Function to build a Max-Heap from the given array
+void buildHeap(int arr[], int n)
+{
+	for (int i = (n-2)/2;i>=0;i--) {
+		maxHeapify(arr, n, i);
+	}
+}
+
+///Heap sort
+/*
+Repeatedly swap root with last node, reduce heap size by one and heapify
+
+*/
+void heapSort(int arr[],int n){
+buildHeap(arr,n);
+for(int i=n-1;i>=1;i--){
+	swap(arr[0],arr[i]);
+	maxHeapify(arr,i,0);
+}
+}
+
+// A utility function to print the array
+// representation of Heap
+void printHeap(int arr[], int n)
+{
+	cout << "Array representation of Max Heap is:\n";
+
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+int main()
+{
+	/*
+			   4
+			 /	 \
+	        10    2
+		  / \	 / \
+	     13 7   14 20
+	    / \
+	    1 9
+	*/
+	int arr[] = { 4,10,2,13,7,14,20,1,9 };
+
+
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	buildHeap(arr, n);
+
+	heapSort(arr,n);
+
+	printHeap(arr, n);
+	// Final Min Heap:
+	//		   20
+	//		 /	 \
+	//     13     14
+	//	 / \	 / \
+	//  10	7   4   2
+	// / \
+	// 1  9
+
+	return 0;
+}
